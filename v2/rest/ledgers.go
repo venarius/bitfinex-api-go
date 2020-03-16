86e5a345be318a7c580a883ae594d98c7cbef6f8
venarius <tim.lange@titanhq.de>
2020-03-16 10:08:05 +0000

    NEW: Added category to ledgers in rest_v2

--- v2/rest/ledgers.go
+++ v2/rest/ledgers.go
@@ -1,9 +1,9 @@
 package rest
 
 import (
+	"fmt"
 	"github.com/venarius/bitfinex-api-go/v2"
 	"path"
-	"fmt"
 )
 
 // LedgerService manages the Ledgers endpoint.
@@ -14,12 +14,17 @@ type LedgerService struct {
 
 // Retrieves all of the past ledger entreies
 // see https://docs.bitfinex.com/reference#ledgers for more info
-func (s *LedgerService) Ledgers(currency string, start int64, end int64, max int32) (*bitfinex.LedgerSnapshot, error) {
-    if max > 500 {
-    	return nil, fmt.Errorf("Max request limit is higher then 500 : %#v", max)
-    }
+func (s *LedgerService) Ledgers(currency string, start int64, end int64, max int32, category int32) (*bitfinex.LedgerSnapshot, error) {
+	if max > 500 {
+		return nil, fmt.Errorf("Max request limit is higher then 500 : %#v", max)
+	}
 
-	req, err := s.requestFactory.NewAuthenticatedRequestWithData(bitfinex.PermissionRead, path.Join("ledgers", currency, "hist"), map[string]interface{}{"start": start, "end": end, "limit": max})
+	var req Request
+	if category != 0 {
+		req, err := s.requestFactory.NewAuthenticatedRequestWithData(bitfinex.PermissionRead, path.Join("ledgers", currency, "hist"), map[string]interface{}{"start": start, "end": end, "limit": max})
+	} else {
+		req, err := s.requestFactory.NewAuthenticatedRequestWithData(bitfinex.PermissionRead, path.Join("ledgers", currency, "hist"), map[string]interface{}{"start": start, "end": end, "limit": max, "category": category})
+	}
 	if err != nil {
 		return nil, err
 	}
